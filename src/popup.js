5cf0d9a3576ac1d4d523fa3702bac91c86e1e703
Stella-Choi <111262557+Stella-Choi@users.noreply.github.com>
2022-09-13 10:52:16 +0900

    Making a simple game with vanilla JavaScript

---
+++ src/popup.js
@@ -0,0 +1,10 @@
+// 'use strict';
+
+// class PopUp {
+//   constructor() {
+//     this.popUp = document.querySelector('.pop-up');
+//     this.popUpText = document.querySelector('.pop-up__message');
+//     this.popUpRefresh = document.querySelector('.pop-up__refresh');
+//     this.popUpRefresh.addEventListener();
+//   }
+// }
